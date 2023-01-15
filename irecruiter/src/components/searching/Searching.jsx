c65e914fb6df666e191a1a94b9da34fa2c73181a
Levon <milevon@gmail.com>
2023-01-15 16:42:57 +0400

    still seraching

--- irecruiter/src/components/searching/Searching.jsx
+++ irecruiter/src/components/searching/Searching.jsx
@@ -3,110 +3,143 @@ import "./SearchingStyles.css";
 import { Button, Divider } from "@mui/material";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { collection, query, where, getDocs  } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../../db/firebase";
-import LinearProgress from '@mui/material/LinearProgress';
-import  uuid  from 'react-uuid';
-
-
-
-
+import LinearProgress from "@mui/material/LinearProgress";
+import uuid from "react-uuid";
 
 const Searching = ({ isSearching, searchInputValue }) => {
-  
-    let valueToSearch = searchInputValue&&searchInputValue[0].toUpperCase() + searchInputValue.slice(1)
-    const [searchResults, setSearchResults] = useState(null);
-    const [searchCollection, setSearchCollection] = useState('candidates')
-    const [loading, setLoading] = useState(false)
-    
-    const Ref = collection(db, searchCollection === 'jobs' ? "jobs" : 'employee');
-    const searchingField = searchCollection === 'jobs'?'title':"Candidate Name"
-  
+  let valueToSearch =
+    searchInputValue &&
+    searchInputValue[0].toUpperCase() + searchInputValue.slice(1);
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchCollection, setSearchCollection] = useState("candidates");
+  const [loading, setLoading] = useState(false);
 
+  const Ref = collection(db, searchCollection === "jobs" ? "jobs" : "employee");
+  const searchingField =
+    searchCollection === "jobs" ? "Category" : "Candidate Name";
 
+  useEffect(() => {
+    let handle = setTimeout(() => {
+      if (valueToSearch.length >= 3) {
+        setLoading(true);
+        async function handleSearch() {
+          let resultData = [];
+          const q = query(Ref, where(searchingField, ">=", valueToSearch));
+          const querySnapshot = await getDocs(q);
+          console.log(querySnapshot);
+          querySnapshot.forEach((doc) => {
+            resultData.push(doc.data());
+          });
+          console.log(resultData);
+          Array.isArray(resultData) && !resultData.length
+            ? setSearchResults("Nothig found")
+            : setSearchResults(resultData);
+          setLoading(false);
+        }
+        handleSearch();
+      }
+    }, 1000);
 
-    useEffect(() => {
-        let handle = setTimeout(() => {
-            if (valueToSearch.length >= 3) {
-                setLoading(true)
-               
-                async function handleSearch() { 
-                    let resultData = []
-                    const q = query(Ref, where(searchingField, ">=", valueToSearch));
-                    const querySnapshot = await getDocs(q);
-                    console.log(querySnapshot)
-                    querySnapshot.forEach((doc) => {
-                        resultData.push(doc.data())
-                    });
-                    console.log(resultData)
-                    Array.isArray(resultData)&&!resultData.length?setSearchResults('Nothig found'):setSearchResults(resultData)
-                    setLoading(false)
-                }
-                handleSearch()
-            }
-          }, 1000);
-          
-          return () => {
-            clearTimeout(handle);
-          }
-       },[searchInputValue, searchCollection])
-   
-    
-    
-    
-    function RenderSearchedResults({searchResults}) {
-      return (
-        <div className="search-result">
-              {searchResults ==='Nothig found'?<div>No results found for the search.</div>:(<>
+    return () => {
+      clearTimeout(handle);
+    };
+  }, [searchInputValue, searchCollection]);
+
+  function RenderSearchedResults({ searchResults }) {
+    console.log(searchingField);
+    return (
+      <div className="search-result">
+        {searchResults === "Nothig found" ? (
+          <div>No results found for the search.</div>
+        ) : (
+          <>
             {searchResults.map((res) => {
-                return <div className="result-line">
-                    <div className="avatar-circel">
-                        <div className="avatar-initial">
-                            <span> {res["Candidate Name"][0]}</span>
-                           </div> 
+              return (
+                <Link
+                  to={
+                    searchCollection === "jobs"
+                      ? `/jobs/${res.id}`
+                      : `/candidate/${res.id}`
+                  }
+                >
+                  <div className="result-line">
+                    <div className="result-left-wrap">
+                      <div className="result-left">
+                        <div className="avatar-circel">
+                          <div className="avatar-initial">
+                            <span>
+                              {" "}
+                              {searchCollection === "jobs"
+                                ? res["Position Name"][0]
+                                : res["Candidate Name"][0]}
+                            </span>
+                          </div>
+                        </div>
+                        <span>
+                          {searchCollection === "jobs"
+                            ? res["Position Name"]
+                            : res["Candidate Name"]}
+                        </span>
+                      </div>
                     </div>
-                    <li key={uuid()}><Link to={`/candidate/${res.id}`}>
-                        <span>{res["Candidate Name"]}</span>
-                        <span className="can-phone">{res['Candidate Phone Number']}</span>
-                    </Link>
-                    </li>
-                </div>;
+
+                    <div className="result-right">
+                      <span>
+                        {searchCollection === "jobs"
+                          ? res["Client"]
+                          : res["Candidate Phone Number"]}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+              );
             })}
-          </>)}
-        </div>
-      );
-    }
-
-
-
+          </>
+        )}
+      </div>
+    );
+  }
 
     const handleClick = (collection) => {
-        setSearchCollection(collection)};
-        
+      
+    setSearchCollection(collection);
+  };
 
-    if (!isSearching) { return }
-    
-    console.log(searchResults)
+  if (!isSearching) {
+    return;
+  }
+
+  console.log(searchResults);
   return (
     <div className="searchResultWrap">
       <div className="filter">
-              <div className="filter-title">
-              {loading && <LinearProgress />}
+        <div className="filter-title">
+          {loading && <LinearProgress />}
           <span>Search for</span>
         </div>
-              <Button
-                  style={searchCollection ==='candidates'?{backgroundColor:'#1976d2', color:'white'}:{}}
+        <Button
+          style={
+            searchCollection === "candidates"
+              ? { backgroundColor: "#1976d2", color: "white" }
+              : {}
+          }
           variant="outlined"
           sx={{ height: "20px" }}
-          onClick={() => handleClick('candidates')}
+          onClick={() => handleClick("candidates")}
         >
           Candidates
         </Button>
-              <Button
-                  style={searchCollection ==='jobs'?{backgroundColor:'#1976d2', color:'white'}:{}}
+        <Button
+          style={
+            searchCollection === "jobs"
+              ? { backgroundColor: "#1976d2", color: "white" }
+              : {}
+          }
           variant="outlined"
           sx={{ height: "20px" }}
-          onClick={() =>handleClick('jobs')}
+          onClick={() => handleClick("jobs")}
         >
           Jobs
         </Button>
@@ -114,7 +147,7 @@ const Searching = ({ isSearching, searchInputValue }) => {
       </div>
       <div className="results">
         {searchResults ? (
-                  <RenderSearchedResults searchResults={ searchResults} />
+          <RenderSearchedResults searchResults={searchResults} />
         ) : (
           <div className="initial-search-tex">
             Please enter 3 characters or more
