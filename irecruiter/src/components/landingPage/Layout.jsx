64f46b8170d16e6c5aadd2568d2565a7a8f8e464
Levon Tonyan <levontonyan87@gmail.com>
2023-01-16 06:47:05 +0000

    css modified

--- irecruiter/src/components/landingPage/Layout.jsx
+++ irecruiter/src/components/landingPage/Layout.jsx
@@ -4,41 +4,29 @@ import { Outlet, Link } from "react-router-dom";
 import HandshakeIcon from "@mui/icons-material/Handshake";
 import { Button } from "@mui/material";
 
-
 function Layout() {
-
-
-
   return (
     <>
       <nav className="landing-navbar">
         <div className="navbar-logo">
           <Link to="/">
             <HandshakeIcon
-              sx={{ width: "60px",height: "80px" }}
+              sx={{ width: "60px", height: "80px" }}
               color="primary"
             />
           </Link>
         </div>
         <div className="navlink">
-         
-            <Link  to="/home">
-              Home
-            </Link>
+          <Link to="/home">Home</Link>
 
-            <Link  to="/about">
-              About
-            </Link>
+          <Link to="/about">About</Link>
 
-            <Link to="/contact">
-              Contact
-            </Link>
-            <Link to="login" >
-                          <Button variant="contained" sx={{backgroundColor:"#1ebd53"}}>
-                          Sign In
-                          </Button>
-            </Link>
-          
+          <Link to="/contact">Contact</Link>
+          <Link to="login">
+            <Button variant="contained" sx={{ backgroundColor: "#1ebd53" }}>
+              Log In
+            </Button>
+          </Link>
         </div>
       </nav>
       <Outlet />
