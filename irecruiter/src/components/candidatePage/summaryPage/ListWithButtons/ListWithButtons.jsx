36ff68ffe50d0dc00db64d7485a85f090b690d17
Levon <milevon@gmail.com>
2023-01-17 21:07:53 +0400

    array in lines added

--- irecruiter/src/components/candidatePage/summaryPage/ListWithButtons/ListWithButtons.jsx
+++ irecruiter/src/components/candidatePage/summaryPage/ListWithButtons/ListWithButtons.jsx
@@ -2,8 +2,15 @@ import "./ListWithButtonsStyles.css";
 import { useState } from "react";
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DoneIcon from '@mui/icons-material/Done';
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc,arrayUnion, arrayRemove,deleteField   } from "firebase/firestore";
 import { db } from './../../../../db/firebase';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
+
+
+
+
 
 
 
@@ -11,12 +18,22 @@ import { db } from './../../../../db/firebase';
 function MiniInput({ input, setInput, setShowMiniEdit, id, field }) {
 
   const candidateRef = doc(db, "employee", id);
+
+
   const handleAdd = () => {
-    updateDoc(candidateRef, {
-      [field]: input
-    }).catch((e) => console.log(e))
-    setInput("");
-    setShowMiniEdit(false);
+    if (field.charAt(field.length - 1) === 's') {
+      updateDoc(candidateRef, {
+        [field]: arrayUnion(input)
+      }).catch((e) => console.log(e))
+      setInput("");
+      setShowMiniEdit(false);
+    } else { 
+      updateDoc(candidateRef, {
+        [field]: input
+      }).catch((e) => console.log(e))
+      setInput("");
+      setShowMiniEdit(false);
+    }
   };
 
   return (
@@ -45,30 +62,65 @@ const AddButton = ({ setShowMiniEdit }) => {
         setShowMiniEdit(true);
       }}
     >
-      + Add
+      +
     </button>
   );
 };
 
 
+
+
+
+
+
 function Editable({ candidate, field }) {
   const [showMiniEdit, setShowMiniEdit] = useState(false);
   const [input, setInput] = useState("");
-
+  
   const handleEdit = () => {
     setShowMiniEdit(true);
   };
+
+  const handleDelete = (name, target) => { 
+    const isFieldPlural = target.charAt(field.length - 1) === 's'
+    console.log(target)
+    const item = doc(db, "employee", candidate.id);
+      updateDoc(item, {
+       
+        [target]: isFieldPlural?arrayRemove(name):deleteField()
+      }).catch(e => console.log(e)).then(() => setInput(""))
+      }
+  
   ////////////////HERE SEND TO SERVER////////////////////////
 
   return (
     <>
       {(candidate[field] && !showMiniEdit) ? (
-        <div>
-          <span> {candidate[field]}</span>
-          <button onClick={handleEdit} className="edit-btn">
-            <ModeEditIcon sx={{height:"16px"}} />
-          </button>
-        </div>
+        
+        //////if name of the field is plural return array of chips//////////////////
+        candidate[field].length&&field.charAt(field.length - 1) === 's'
+          ? <Stack direction="row" spacing={1}>{
+            candidate[field].map(el => { 
+              return ( <Chip
+                label={el}
+                size='small'
+                onDelete={() => handleDelete(el,field)}
+                onClick={handleEdit} />)
+            })
+          }
+            <AddButton candidate={candidate} setShowMiniEdit={setShowMiniEdit}/>
+          </Stack>
+        ////////Else just chip//////////////////  
+          : <>
+              {!Array.isArray(candidate[field])?<Chip
+              label={candidate[field]}
+              onClick={handleEdit}
+              size='small'
+              onDelete={() => handleDelete(null, field)} />:<AddButton candidate={candidate} setShowMiniEdit={setShowMiniEdit} />}
+            </>
+          
+        
+        
       ) : showMiniEdit ? (
         <MiniInput
             input={input}
