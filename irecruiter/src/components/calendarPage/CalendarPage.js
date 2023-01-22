457b799a3e466ad0e9e149efd951c6f5d7eae9e0
gaya94 <gayagrig1994@gmail.com>
2023-01-22 14:58:04 +0400

    calendar changed

--- irecruiter/src/components/calendarPage/CalendarPage.js
+++ irecruiter/src/components/calendarPage/CalendarPage.js
@@ -1,29 +1,78 @@
-import './CalendarPage.css'
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { UserAuth } from '../../context/AuthContext';
 
 
-function CalendarPage() {
- const [value, setValue] = React.useState(new Date());
+let eventGuid = 0
 
+function createEventId() {
+  return String(eventGuid++)
+}
+
+const CalendarPage=()=>{
+  const {isSidebarOpen} = UserAuth()
+
+  
   return (
-      <div className='calendar-main'> 
-        <LocalizationProvider dateAdapter={AdapterMoment}>
-            <StaticDatePicker
-            orientation="portrait"
-            openTo="day"
-            value={value}
-            onChange={(newValue) => {
-                setValue(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} />}
-        />
-        </LocalizationProvider>
+    <div className={isSidebarOpen ? 'sideBarOpen' : null}>
+      <FullCalendar
+        height={'80vh'}
+        plugins={[dayGridPlugin,interactionPlugin]}
+        headerToolbar={{
+          start:"prev,next",
+          center:"title",
+          end:"dayGridMonth, dayGridWeek, dayGridDay",
+        }}
+        initialView='dayGridMonth'
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        select={handleDateSelect}
+        eventContent={renderEventContent} // custom render function
+        eventClick={handleEventClick}            
+        
+      />
+      
     </div>
   )
 }
 
+const handleDateSelect = (selectInfo) => {
+  let title = prompt('Enter event')
+  let calendarApi = selectInfo.view.calendar
+
+  calendarApi.unselect() // clear date selection
+
+  if (title) {
+    calendarApi.addEvent({
+      id: createEventId(),
+      title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay
+    })
+  }
+}
+
+const handleEventClick = (clickInfo) => {
+  if ((`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    clickInfo.event.remove()
+  }
+}
+
+function renderEventContent(eventInfo) {
+return (
+  <>
+    <b>{eventInfo.timeText}</b>
+    <i>{eventInfo.event.title}</i>
+  </>
+)
+}
 export default CalendarPage
+
+
+
+
